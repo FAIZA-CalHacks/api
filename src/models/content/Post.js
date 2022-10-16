608495240e940f2ca3c421994d5be65d7c3faa88
Daniel <79312811+danieljbk@users.noreply.github.com>
2022-10-16 06:47:01 -0700

    removed investments in post

--- src/models/content/Post.js
+++ src/models/content/Post.js
@@ -6,11 +6,6 @@ const PostSchema = new Schema({
     createdAt: { type: Date, default: Date.now },
     author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    investments: {
-      type: [Schema.Types.ObjectId],
-      ref: 'Investment',
-      default: [],
-    },
   },
   title: { type: String, trim: true, required: true },
   body: { type: String, trim: true, required: true },
